07a6c2eee5c302ba881500942c82927a1ef95aae
Alvaro Bautista <alvarobp@gmail.com>
2024-09-24 00:24:37 +0200

    Apply ssh-agent socket fix only on Arch

--- dot_bash/config/ssh-agent.sh
+++ dot_bash/config/ssh-agent.sh
@@ -1,4 +1,4 @@
-if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
+if cat /etc/os-release | grep -q "ID=arch" && [[ ! -f "$SSH_AUTH_SOCK" ]]; then
   export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
 ssh-add -A 2>/dev/null
