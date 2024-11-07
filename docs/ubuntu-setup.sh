c82a8f5b19aa04f40607e1765246a941644fc2c6
Alvaro Bautista <alvarobp@gmail.com>
2024-11-07 22:24:01 +0100

    Add ubuntu setup script to docs

---
+++ docs/ubuntu-setup.sh
@@ -0,0 +1,148 @@
+# Base
+sudo apt update -y
+sudo apt install -y \
+	git curl \
+	build-essential pkg-config autoconf \
+  libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev \
+  ca-certificates
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Desktop
+sudo apt install -y \
+  gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager \
+  flameshot \
+  gir1.2-gtop-2.0
+
+sudo add-apt-repository ppa:numix/ppa
+sudo apt install -y numix-icon-theme-circle
+
+# Fonts
+mkdir -p ~/.local/share/fonts
+
+cd ~/Downloads
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+unzip CascadiaMono.zip -d CascadiaFont
+cp CascadiaFont/*.ttf ~/.local/share/fonts
+rm -rf CascadiaMono.zip CascadiaFont
+
+fc-cache
+
+# Tools
+sudo apt install -y tmux gcalcli pavucontrol
+brew install fzf
+
+# Terminal
+sudo apt install -y ripgrep bat eza zoxide btop apache3-utils
+
+# Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker.io docker-buildx
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ${USER}
+
+cd ~/Downloads
+
+# 1Password App & CLI
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+sudo apt update && sudo apt install -y 1password 1password-cli
+
+# Firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt-get update && sudo apt-get install -y firefox
+
+# Github CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+  sudo apt update &&
+  sudo apt install gh -y
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Slack
+wget https://downloads.slack-edge.com/desktop-releases/linux/x64/4.37.101/slack-desktop-4.37.101-amd64.deb
+sudo dpkg -i slack-desktop-4.37.101-amd64.deb
+
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install -y spotify-client
+
+# Visual Studio Code
+wget -O code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+sudo apt install -y ./code.deb
+rm code.deb
+
+# Zoom
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install -y ./zoom_amd64.deb
+rm zoom_amd64.deb
+
+# Mise
+sudo apt update -y && sudo apt install -y gpg sudo wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
+
+mise use --yes --global awscli@latest
+mise use --yes --global aws-vault@latest
+mise use --yes --global gcloud@latest
+gcloud components update --quiet
+gcloud components install --quiet beta
+mise use --yes --global mc@latest
+mise use --yes --global nodejs@20.10.0
+mise use --yes --global pnpm@9.1.2
+mise plugins install terraform https://github.com/asdf-community/asdf-hashicorp.git
+mise use --yes --global terraform@1.8.3
+
+mise use --yes --global lazygit@latest
+
+# Neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
+sudo apt install -y neovim
+
+# ASDF
+# git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+# asdf plugin-add awscli
+# asdf install awscli
+# asdf install awscli latest
+# asdf plugin-add aws-vault
+# asdf install aws-vault latest
+# asdf plugin-add gcloud
+# asdf install gcloud latest
+# gcloud components update
+# gcloud components install beta
+# asdf plugin-add mc
+# asdf install mc latest
+# asdf plugin-add nodejs
+# asdf install nodejs 20.10.0
+# asdf plugin-add pnpm
+# asdf install pnpm 9.1.2
+# asdf plugin-add terraform
+# asdf install terraform 1.8.3
