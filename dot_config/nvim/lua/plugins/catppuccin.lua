05ea7eef7a7844df270674567e6df9a209797490
Alvaro Bautista <alvarobp@gmail.com>
2024-11-08 11:44:54 +0100

    Use catppuccin-mocha in neovim

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -2,8 +2,8 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  lazy = false,
   config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
+    require("catppuccin").setup()
+    vim.cmd.colorscheme "catppuccin"
   end
 }
