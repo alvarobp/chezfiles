4060b6adf7e7145eb054e1a624916a0c841dfb49
Álvaro Bautista Pino <alvarobp@users.noreply.github.com>
2025-01-08 09:42:34 +0100

    Update spotify debian repository GPG key in ubuntu setup doc

--- docs/ubuntu-setup.sh
+++ docs/ubuntu-setup.sh
@@ -91,7 +91,7 @@ wget https://downloads.slack-edge.com/desktop-releases/linux/x64/4.37.101/slack-
 sudo dpkg -i slack-desktop-4.37.101-amd64.deb
 
 # Spotify
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt install -y spotify-client
