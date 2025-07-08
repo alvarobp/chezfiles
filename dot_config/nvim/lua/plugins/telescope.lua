4b95f37ef2acb9df9aab5af73dd4a6580ffed831
Alvaro Bautista <alvarobp@gmail.com>
2025-07-08 11:48:08 +0200

    Configure telescope to grep on hidden files

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -20,6 +20,16 @@ return {
         defaults = {
           file_ignore_patterns = {
             "node_modules"
+          },
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
           }
         }
       }
