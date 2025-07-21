4ccc62279301f0e8b55123a72e74369b03080d02
Alvaro Bautista <alvarobp@gmail.com>
2025-07-21 19:13:52 +0200

    Account for missing ssh directory in user home

--- dot_bash/config/ssh-agent.sh
+++ dot_bash/config/ssh-agent.sh
@@ -1,5 +1,6 @@
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
   eval "$(ssh-agent -s)" >/dev/null
+  mkdir -p ~/.ssh
   ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
