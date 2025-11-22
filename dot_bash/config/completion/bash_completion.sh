84cf1d00377ed2396b57ea25a50bc48d74446b4d
Alvaro Bautista <alvarobp@gmail.com>
2025-11-22 16:48:54 -0500

    Support bash completion in macos

--- dot_bash/config/completion/bash_completion.sh
+++ dot_bash/config/completion/bash_completion.sh
@@ -1,6 +1,15 @@
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
+if ! shopt -oq posix; then
+    # Check for macOS Homebrew bash completion (Apple Silicon)
+    if [ -f /opt/homebrew/etc/bash_completion ]; then
+        . /opt/homebrew/etc/bash_completion
+    # Check for macOS Homebrew bash completion (Intel)
+    elif [ -f /usr/local/etc/bash_completion ]; then
+        . /usr/local/etc/bash_completion
+    # Check for Linux bash completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
 fi
--- dot_bash/config/git.sh
+++ dot_bash/config/git.sh
@@ -1,3 +1,10 @@
-if [ -f /usr/share/bash-completion/completions/git ]; then
+# Check for macOS Homebrew git completion (Apple Silicon)
+if [ -f /opt/homebrew/etc/bash_completion.d/git-completion.bash ]; then
+  source /opt/homebrew/etc/bash_completion.d/git-completion.bash
+# Check for macOS Homebrew git completion (Intel)
+elif [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
+  source /usr/local/etc/bash_completion.d/git-completion.bash
+# Check for Linux git completion
+elif [ -f /usr/share/bash-completion/completions/git ]; then
   source /usr/share/bash-completion/completions/git
 fi
